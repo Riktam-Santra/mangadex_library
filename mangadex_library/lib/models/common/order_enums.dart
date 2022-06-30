c9a557b017ed2b410c13a1b8e21ccddb6d7b915b
Riktam Santra <riktamsantra56@gmail.com>
2022-07-01 00:25:03 +0530

    Enums to organise order options for a function

---
+++ mangadex_library/lib/models/common/order_enums.dart
@@ -0,0 +1,38 @@
+enum SearchOrders {
+  title,
+  year,
+  createdAt,
+  updatedAt,
+  latestUploadedChapter,
+  followedCount,
+  relevance,
+  rating,
+}
+
+enum ChapterOrders {
+  createdAt,
+  updatedAt,
+  publishAt,
+  readableAt,
+  volume,
+  chapter,
+}
+
+enum CoverOrders {
+  createdAt,
+  updatedAt,
+  volume,
+}
+
+enum OrderDirections {
+  ascending,
+  descending,
+}
+
+enum ScanlationOrders {
+  name,
+  createdAt,
+  updatedAt,
+  followedCount,
+  relevance,
+}
